100eb3e4a07efff225eba363e1209a1ccf666c73
Michael Fellinger <michael.fellinger@iohk.io>
2019-09-12 11:56:08 +0200

    Add sources.nix from iohk-ops

--- nix/default.nix
+++ nix/default.nix
@@ -7,6 +7,10 @@ with {
     nodejs = super.nodejs-slim-11_x;
     inherit (import sources.yarn2nix { pkgs = self; }) mkYarnPackage yarn2nix;
     packages = self.callPackages ./packages.nix { };
+    iohkNix = import sources.iohk-nix {
+      application = "jormungandr-faucet";
+      nixpkgsOverride = self;
+    };
 
     rustc = super.latest.rustChannels.stable.rust.override {
       targets = [ "wasm32-unknown-unknown" ];
